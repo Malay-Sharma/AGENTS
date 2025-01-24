a00f23564fd6b18166c97709b3cba51e06735c14
Malay-Sharma <malaysharma0001@gmail.com>
2025-01-24 17:05:44 +0530

    Adding State management via 'use' hook (React 19)

--- agent1/app/dashboard/layout.tsx
+++ agent1/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Header from "@/components/Header"
+import { NavigationProvider } from "@/lib/NavigationProvider";
 import { Authenticated } from "convex/react";
 
 export default function DashboardPage({
@@ -9,17 +10,19 @@ export default function DashboardPage({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex h-screen">
-            <Authenticated>
-                <h1>Sidebar</h1>
-                {/* <Sidebar /> */}
-            </Authenticated>
+        <NavigationProvider>
+            <div className="flex h-screen">
+                <Authenticated>
+                    <h1>Sidebar</h1>
+                    {/* <Sidebar /> */}
+                </Authenticated>
 
-            <div className="flex-1 flex flex-col min-w-0">
-                <Header />
-                <main>{children}</main>
+                <div className="flex-1 flex flex-col min-w-0">
+                    <Header />
+                    <main>{children}</main>
+                </div>
             </div>
-        </div>
+        </NavigationProvider>
 
     )
 
